e3e23ac76c17428b38c2ff56eef9716de8ca511d
thomas turner <tdturner@ucdavis.edu>
2020-06-20 01:50:50 -0700

    backend/models/note.js: Connect to MongoDB Atlas
    Signed-off-by: thomas turner <tdturner@ucdavis.edu>

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
-const url      = process.env.MONGODB_URI
+const {readFileSync} = require('fs');
+const url      = process.env.MONGODB_ATLAS_URI;
+const options  = {
+	//https://docs.mongodb.com/manual/reference/connection-string/#connection-string-options 
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	w: "majority",
+	retryWrites: true,
+//	ssl: true,
+//	authMechanism: "MONGODB-X509",
+//	authSource: "$external",
+//	tlsCertificateKeyFile: readFileSync(process.env.MONGODB_X509_PATH)
+};
 
 console.log('connection to', url);
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(url, options)
 	.then(result => {
-    		console.log('connected to MongoDB')
+    		console.log('connected to MongoDB');
   	})
   	.catch((error) => {
-    		console.log('error connecting to MongoDB:', error.message)
+		console.error(error);
+    		console.log('error connecting to MongoDB:', error.message);
   	});
 
 const noteSchema = new mongoose.Schema({
