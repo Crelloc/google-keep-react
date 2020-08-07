c41e39f6c8da8c82a3580c9c05a76ba1461e3611
thomas turner <thomastdt@gmail.com>
2020-08-07 11:59:29 -0700

    src/components/Note.jsx: Removed unused Components
    Signed-off-by: thomas turner <thomastdt@gmail.com>

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
-import { Zoom } from "@material-ui/core/";
 
 
 function Note(props) {
