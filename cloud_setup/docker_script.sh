a94038a6f4315874fe7de9cf3089f721470fc6b1
thomas turner <thomastdt@gmail.com>
2020-08-09 11:50:59 -0700

    cloud_setup/docker_script.sh: add cmd to .bashrc
    Signed-off-by: thomas turner <thomastdt@gmail.com>

--- cloud_setup/docker_script.sh
+++ cloud_setup/docker_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #better docker processing formatting
-echo 'export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> ./test
-echo 'alias dps="docker ps -a --format=$FORMAT"' >> ./test
+echo 'export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> /home/$USER/.bashrc
+echo 'alias dps="docker ps -a --format=$FORMAT"' >> /home/$USER/.bashrc
 
 #add user to docker group
 usermod -aG docker $USER
