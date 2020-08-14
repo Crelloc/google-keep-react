20120d8beff846cad45a02f740b78ca297f9c8b0
Thomas D Turner <thomastdt@gmail.com>
2020-08-14 14:14:26 -0700

    run_firewall.sh: correct systemd permissions

--- cloud_setup/run_firewall.sh
+++ cloud_setup/run_firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 664 custom_firewall.service
+chmod 0644 custom_firewall.service
 chmod 744 firewallsetup/firewall*
 cp custom_firewall.service /etc/systemd/system
 cp -R firewallsetup /etc
