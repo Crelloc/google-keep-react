865f4ec3a7f75f819fdbcc33b44a89234fe3d9e2
Ubuntu <ubuntu@ip-172-31-6-31.us-west-1.compute.internal>
2020-08-16 05:09:33 +0000

    Docker Notes
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-6-31.us-west-1.compute.internal>

--- cloud_setup/docker_script.sh
+++ cloud_setup/docker_script.sh
@@ -3,6 +3,13 @@
 #better docker processing formatting
 echo 'export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> /home/$USER/.bashrc
 echo 'alias dps="docker ps -a --format=$FORMAT"' >> /home/$USER/.bashrc
+source .bashrc
 
-#add user to docker group
-usermod -aG docker $USER
+## Build docker image from Dockerfile located in current directory
+#docker build -t keeper:node-latest .
+
+## Docker command to run container image
+#docker run -p 80:5000 -p 27017:27017 -d --name keeper --restart unless-stopped keeper:node-latest
+
+## Add user to docker group
+#usermod -aG docker $USER
