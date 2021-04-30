3f964320015c6e5c3347a769ea8cfc8fb390c7cb
thomas turner <thomastdt@gmail.com>
2021-04-29 20:07:23 -0700

    Updating for csp
    Signed-off-by: thomas turner <thomastdt@gmail.com>

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const path    = require('path');
-const cluster = require('cluster');
-const bodyParser = require('body-parser');
-const Note    = require('./models/note');
-const numCPUs = require('os').cpus().length;
-const isDev   = process.env.NODE_ENV !== 'production';
-const PORT    = 5000;
+require("dotenv").config();
+const express = require("express");
+// const helmet = require("helmet");
+const path = require("path");
+const cluster = require("cluster");
+// const bodyParser = require("body-parser");
+const Note = require("./models/note");
+
+const numCPUs = require("os").cpus().length;
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = 5000;
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -17,91 +19,118 @@ if (!isDev && cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
   });
-
 } else {
   const app = express();
-  //Middleware for JSON 
-  app.use(bodyParser.json());
+  //Middleware for JSON
+  //   app.use(bodyParser.json());
+  app.use(express.json());
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../build')));
+  app.use(express.static(path.resolve(__dirname, "../build")));
 
   // Answer API requests.
 
-  app.put('/api/notes/:id', (request, response) => {
-  	const { title, content } = request.body;
-	const note = {
-		title: title,
-		content: content
-	};
-	console.log(request.body);
+  app.put("/api/notes/:id", (request, response) => {
+    const { title, content } = request.body;
+    const note = {
+      title: title,
+      content: content,
+    };
+    console.log(request.body);
 
-	Note.findByIdAndUpdate(request.params.id, note, {new: true})
-	  .then(updatedNote => {
-	  	response.json(updatedNote);
-	  })
-	  .catch(err => {
-	  	console.error(err);
-		response.status(400).send({ error: 'malformatted id' });
-	  });
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then((updatedNote) => {
+        response.json(updatedNote);
+      })
+      .catch((err) => {
+        console.error(err);
+        response.status(400).send({ error: "malformatted id" });
+      });
   });
-  app.delete('/api/notes/:id', (request, response) => {
-	Note.findByIdAndRemove(request.params.id)
-	  .then(result => {
-	  	response.status(204).end();
-	  })
-	  .catch(err => { //simple error handling for now
-	  	console.error(err);
-		response.status(404).end();
-	  });
-  });
-
-  app.post('/api/notes', (request, response) => {
-	const note = request.body;
-
-	new Note({
-		title  : note.title,
-		content: note.content,
-		date   : new Date()
-	})
-	  .save()
-	  .then(savedNote => {
-  		response.json(savedNote);
-	  })
+  app.delete("/api/notes/:id", (request, response) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then((result) => {
+        response.status(204).end();
+      })
+      .catch((err) => {
+        //simple error handling for now
+        console.error(err);
+        response.status(404).end();
+      });
   });
 
-  app.get('/api/notes/:id', (request, response) => {
-	Note.findById(request.params.id)
-	  .then(note => {
-		if(note){
-    			response.json(note);
-		} else {
-			response.status(404).end();
-		}
-  	  })
-	  .catch(err => {
-	  	console.error(err);
-		response.status(400).send({error: 'malformatted id'});
-	  });
+  app.post("/api/notes", (request, response) => {
+    const note = request.body;
+
+    new Note({
+      title: note.title,
+      content: note.content,
+      date: new Date(),
+    })
+      .save()
+      .then((savedNote) => {
+        response.json(savedNote);
+      });
   });
 
-  app.get('/api/notes', function (request, response) {
-   /* res.set('Content-Type', 'application/json');
+  app.get("/api/notes/:id", (request, response) => {
+    Note.findById(request.params.id)
+      .then((note) => {
+        if (note) {
+          response.json(note);
+        } else {
+          response.status(404).end();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        response.status(400).send({ error: "malformatted id" });
+      });
+  });
+
+  app.get("/api/notes", function (request, response) {
+    /* res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
     */
-    Note.find({}).then(notes => {
-    	response.json(notes);
+    Note.find({}).then((notes) => {
+      response.json(notes);
     });
   });
 
+  // app.use((req, res, next) => {
+  //   // nonce should be base64 encoded
+  //   res.locals.styleNonce = crypto.randomBytes(32).toString("hex");
+  //   next();
+  // });
+
+  // app.use(
+  //   helmet.contentSecurityPolicy({
+  //     directives: {
+  //       defaultSrc: ["'self'"],
+  //       objectSrc: ["'none'"],
+  //       baseUri: ["'none'"],
+  //       styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.styleNonce}'`],
+  //       scriptSrc: ["'strict-dynamic'", "'report-sample'", "https:", "http:"],
+  //     },
+  //   })
+  // );
   // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "../build", "index.html"));
+    // response.render(path.resolve(__dirname, "../build", "index.html"), {
+    //   styleNonce: res.locals.styleNonce,
+    // });
   });
 
   app.listen(PORT, function () {
-    console.log(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+    console.log(
+      `Node ${
+        isDev ? "dev server" : "cluster worker " + process.pid
+      }: listening on port ${PORT}`
+    );
   });
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,8 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 import Modal from "./Modal";
 import axios from "axios";
-import { Grow, Zoom} from '@material-ui/core';
+import { Grow, Zoom } from "@material-ui/core";
+import styles from "./App.modules.css";
 
 function App() {
   console.log("App Component Rendered!");
@@ -15,75 +16,79 @@ function App() {
   const [editNote, setEditNote] = useState({});
 
   useEffect(() => {
-	axios.get('/api/notes')
-	    .then(response => setNotes(response.data));
+    axios.get("/api/notes").then((response) => setNotes(response.data));
   }, []); // useEffect with '()' and '[]' parameters: initial call to backend once.
 
   function addNote(newNote) {
-	axios.post('/api/notes', newNote)
-          .then(response => {
-    		setNotes(prevNotes => {
-      			return [...prevNotes, response.data];
-    		});
-          });
+    axios.post("/api/notes", newNote).then((response) => {
+      setNotes((prevNotes) => {
+        return [...prevNotes, response.data];
+      });
+    });
   }
   function putNote(data) {
-    axios.put(`/api/notes/${data.id}`, data)
-        .then(response => {
-          console.log(response.data);
-        })
-      .then(response => axios.get('/api/notes'))
-      .then(response => setNotes(response.data))
+    axios
+      .put(`/api/notes/${data.id}`, data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .then((response) => axios.get("/api/notes"))
+      .then((response) => setNotes(response.data))
       .catch((err) => console.error(err));
-    }
+  }
   function deleteNote(id) {
-    axios.delete(`/api/notes/${id}`)
-	  .then(response => axios.get('/api/notes'))
-    .then(response => setNotes(response.data));
+    axios
+      .delete(`/api/notes/${id}`)
+      .then((response) => axios.get("/api/notes"))
+      .then((response) => setNotes(response.data));
   }
 
-  function handleMouseUp(e){
-    console.log("%c mouseup even app.jsx", "color: red;", e)
+  function handleMouseUp(e) {
+    console.log("%c mouseup even app.jsx", "color: red;", e);
     var container = document.getElementById("i-form");
 
-    (container.contains(e.target)) ? setExpand(true)
-                                   : setExpand(false);
+    container.contains(e.target) ? setExpand(true) : setExpand(false);
     console.log(e.target);
   }
 
   function editData(data) {
-      setModal(true);
-      setEditNote(data);
+    setModal(true);
+    setEditNote(data);
   }
 
   return (
-    <div style={{position: "relative", width: "100vw"}}>
-    { !modal && <div onMouseUp={handleMouseUp}>
-      <Header />
-      <CreateArea formId={"i-form"} onAdd={addNote} expand={expand} />
-      <div className="flex">
-
-      {notes.map((noteItem, index) => {
-        return (
-
-          (<Zoom in={true} >
-              <Note
-                key={noteItem.id}
-                data={noteItem}
-                onDelete={deleteNote}
-                editData={editData}
-                />
-            </Zoom>)
-        );
-      })}
-	</div>
-      <Footer />
-    </div> }
-    {modal &&
-        <Grow in={true} style={{ transformOrigin: '0 0 0' }} timeout='auto' children>
-            <Modal editNote={editNote} putNote={putNote} setModal={setModal}/>
+    <div className={styles.app_container}>
+      {!modal && (
+        <div onMouseUp={handleMouseUp}>
+          <Header />
+          <CreateArea formId={"i-form"} onAdd={addNote} expand={expand} />
+          <div className="flex">
+            {notes.map((noteItem, index) => {
+              return (
+                <Zoom in={true}>
+                  <Note
+                    key={noteItem.id}
+                    data={noteItem}
+                    onDelete={deleteNote}
+                    editData={editData}
+                  />
+                </Zoom>
+              );
+            })}
+          </div>
+          <Footer />
+        </div>
+      )}
+      {modal && (
+        <Grow
+          in={true}
+          style={{ transformOrigin: "0 0 0" }}
+          timeout="auto"
+          children
+        >
+          <Modal editNote={editNote} putNote={putNote} setModal={setModal} />
         </Grow>
-        }
+      )}
     </div>
   );
 }
