3602138131aa7ef937282d6694a1d836ee256deb
Thomas Turner <thomastdt@gmail.com>
2023-11-16 06:10:00 -0800

    mongodb docker: Now anyone can clone and run docker compose
    in the main/master branch I have my database in the cloud using
    atlas mongodb.
    
    in this branch I now have my database inside a container.
    
    this allows anyone to clone this repo and run the app for
    themselves without needing my mongo atlas credentials
    
    Signed-off-by: Thomas Turner <thomastdt@gmail.com>

--- cloud_setup/docker_script.sh
+++ cloud_setup/docker_script.sh
@@ -3,13 +3,22 @@
 #better docker processing formatting
 echo 'export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"' >> /home/$USER/.bashrc
 echo 'alias dps="docker ps -a --format=$FORMAT"' >> /home/$USER/.bashrc
-source .bashrc
+source ~/.bashrc
 
 ## Build docker image from Dockerfile located in current directory
 #docker build -t keeper:node-latest .
 
-## Docker command to run container image
+## Example Docker command to run container image
 #docker run -p 80:5000 -p 27017:27017 -d --name keeper --restart unless-stopped keeper:node-latest
 
 ## Add user to docker group
 #usermod -aG docker $USER
+
+#clean up all docker images and volumes
+docker stop $(docker ps -a -q)
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+
+#create docker images and run them
+
+docker compose -f docker-compose.yml up --build --force-recreate --remove-orphans backend mongodb nginx
